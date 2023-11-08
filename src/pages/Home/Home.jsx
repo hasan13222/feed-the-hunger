75cf397f6cdb2018756b58ee293bf6de613f2bca
hasan13222 <jamil8305@gmail.com>
2023-11-08 17:12:28 +0600

    home page title updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,10 +3,14 @@ import './Home.css'
 import Feature from "../../components/Home/Feature"
 import Partners from "../../components/Home/Partners"
 import Members from "../../components/Home/Members"
+import { Helmet } from "react-helmet"
 
 const Home = () => {
   return (
     <>
+    <Helmet>
+      <title>FeedTheHunger | Home</title>
+    </Helmet>
       <Hero/>
       <Feature/>
       <Members/>
