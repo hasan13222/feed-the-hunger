2be047b795bdf83d60b5fb15f29226a3119c05f8
hasan13222 <jamil8305@gmail.com>
2023-11-08 12:54:18 +0600

    dynamic title for routes by react-helmet initiated

--- src/pages/Foods/Foods.jsx
+++ src/pages/Foods/Foods.jsx
@@ -1,24 +1,25 @@
 import { useEffect, useRef, useState } from "react";
 import FoodItem from "../../components/Shared/FoodItem";
-import {BiSort} from 'react-icons/bi'
+import { BiSort } from "react-icons/bi";
+import { Helmet } from "react-helmet";
 
 const Foods = () => {
   const [allFood, setAllFood] = useState([]);
   const [availableFoods, setAvailableFood] = useState([]);
-  
+
   const formRef = useRef();
 
   const handleSearch = (e) => {
     e.preventDefault();
     const searchValue = e.target.search.value;
     console.log(searchValue);
-    const result = allFood?.filter(item => {
-      const foodName = item.foodName.toLowerCase();      
+    const result = allFood?.filter((item) => {
+      const foodName = item.foodName.toLowerCase();
       return foodName.includes(searchValue);
     });
-    
+
     setAvailableFood(result);
-  }
+  };
 
   const handleLessExpireTime = () => {
     const foods = [...availableFoods].sort((a, b) => {
@@ -27,16 +28,16 @@ const Foods = () => {
       return expTimeA - expTimeB;
     });
     setAvailableFood(foods);
-  }
+  };
 
   const handleMoreExpireTime = () => {
-    const foods = [...availableFoods].sort((a, b) => {      
+    const foods = [...availableFoods].sort((a, b) => {
       const expTimeA = new Date(a.expTime);
       const expTimeB = new Date(b.expTime);
       return expTimeB - expTimeA;
     });
     setAvailableFood(foods);
-  }
+  };
 
   useEffect(() => {
     fetch("http://localhost:5000/foods")
@@ -48,6 +49,9 @@ const Foods = () => {
   }, []);
   return (
     <>
+      <Helmet>
+        <title>FeedTheHunger | Available Foods</title>
+      </Helmet>
       <div className="bg-gray-200">
         <div className="container mx-auto">
           <div className="heading">
@@ -55,24 +59,42 @@ const Foods = () => {
               Available Foods
             </h2>
           </div>
-          <div className="filter flex gap-10 justify-center mb-8" >
+          <div className="filter flex gap-10 justify-center mb-8">
             <form ref={formRef} onSubmit={handleSearch} className="w-[40%]">
-                <input className="p-3 mr-1 rounded-md w-[80%]" type="text" name="search" placeholder="Search Here..." />
-                <input className="btn bg-green-700 text-white" type="submit" value="Search" />
+              <input
+                className="p-3 mr-1 rounded-md w-[80%]"
+                type="text"
+                name="search"
+                placeholder="Search Here..."
+              />
+              <input
+                className="btn bg-green-700 text-white"
+                type="submit"
+                value="Search"
+              />
             </form>
-            <button onClick={handleLessExpireTime} className="sort flex items-center btn bg-green-700 text-white">
-                Less Expire Time
-                <BiSort/>
+            <button
+              onClick={handleLessExpireTime}
+              className="sort flex items-center btn bg-green-700 text-white"
+            >
+              Less Expire Time
+              <BiSort />
             </button>
-            <button onClick={handleMoreExpireTime} className="sort flex items-center btn bg-green-700 text-white">
-                More Expire Time
-                <BiSort/>
+            <button
+              onClick={handleMoreExpireTime}
+              className="sort flex items-center btn bg-green-700 text-white"
+            >
+              More Expire Time
+              <BiSort />
             </button>
-            <button onClick={() => {
-              setAvailableFood(allFood);              
-              formRef.reset();
-            }} className="clear btn bg-gray-50">
-                Clear
+            <button
+              onClick={() => {
+                setAvailableFood(allFood);
+                formRef.reset();
+              }}
+              className="clear btn bg-gray-50"
+            >
+              Clear
             </button>
           </div>
           <div className="flex flex-wrap justify-between">
