272bcc548c66e08d432b40355a51d619faf076a6
hasan13222 <jamil8305@gmail.com>
2023-11-08 12:44:57 +0600

    framer motion implemented in the hero section

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,18 +1,31 @@
-
+import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <>
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1 }}
+      >
         <div className="hero">
-            <div className="container py-12">
-                <h1 className="text-5xl leading-snug font-bold text-white w-[750px]">Building Bridges, Sharing Plates: Nourishing Our Community Together</h1>
-                <div className="btns mt-2">
-                    <button className="m-3 btn rounded-sm bg-slate-300">Share Food</button>
-                    <button className="m-3 btn rounded-sm bg-slate-300">Request Food</button>
-                </div>
+          <div className="container py-12">
+            <h1 className="text-5xl leading-snug font-bold text-white w-[750px]">
+              Building Bridges, Sharing Plates: Nourishing Our Community
+              Together
+            </h1>
+            <div className="btns mt-2">
+              <button className="m-3 btn rounded-sm bg-slate-300">
+                Share Food
+              </button>
+              <button className="m-3 btn rounded-sm bg-slate-300">
+                Request Food
+              </button>
             </div>
+          </div>
         </div>
+      </motion.div>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
